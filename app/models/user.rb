2515aea8f398a9ff445a777095b87fff713c2c95
dyoshida1352 <d.yoshida1352@gmail.com>
2021-07-18 05:35:39 +0000

    ユーザー認証機能のレイアウト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #新規登録時の年代選択
+  enum age: {"--未選択--": 0,"10代": 1,"20代": 2,"30代": 3,"40代": 4,"50代": 5,"60代以上": 6}
+
+   #バリデーション
+  validates :name, :age,  presence: true
 end
