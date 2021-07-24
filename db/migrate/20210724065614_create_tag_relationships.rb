10113d7b770d7273bb9e594169cd528459df1ad4
dyoshida1352 <d.yoshida1352@gmail.com>
2021-07-24 08:02:09 +0000

    アイデア投稿のタグ付け機能実装

---
+++ db/migrate/20210724065614_create_tag_relationships.rb
@@ -0,0 +1,13 @@
+class CreateTagRelationships < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tag_relationships do |t|
+
+      t.references :post, foreign_key: true
+      t.references :invite, foreign_key: true
+      t.references :tag, foreign_key: true
+
+      t.timestamps
+    end
+      add_index :tag_relationships, [:post_id, :invite_id, :tag_id],unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_050312) do
+ActiveRecord::Schema.define(version: 2021_07_24_065614) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,13 +22,6 @@ ActiveRecord::Schema.define(version: 2021_07_22_050312) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "favorites", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "invite_comments", force: :cascade do |t|
@@ -79,12 +72,16 @@ ActiveRecord::Schema.define(version: 2021_07_22_050312) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tag_maps", force: :cascade do |t|
+  create_table "tag_relationships", force: :cascade do |t|
     t.integer "post_id"
     t.integer "invite_id"
     t.integer "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["invite_id"], name: "index_tag_relationships_on_invite_id"
+    t.index ["post_id", "invite_id", "tag_id"], name: "index_tag_relationships_on_post_id_and_invite_id_and_tag_id", unique: true
+    t.index ["post_id"], name: "index_tag_relationships_on_post_id"
+    t.index ["tag_id"], name: "index_tag_relationships_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
