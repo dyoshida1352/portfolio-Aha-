11df5a43c1b73a9aefabaf25b3f7a94cca0b56c1
dyoshida1352 <d.yoshida1352@gmail.com>
2021-07-25 10:01:19 +0000

    タグ検索機能実装

--- app/controllers/users/searchs_controller.rb
+++ app/controllers/users/searchs_controller.rb
@@ -10,49 +10,20 @@ class Users::SearchsController < ApplicationController
 
   private
 
-  def match(model, value)
-    if model == 'post'
-      Post.where(post_name: value)
-    elsif model == 'invite'
-      Invite.where(invite_name: value)
-    end
-  end
-
-  def forward(model, value)
-    if model == 'post'
-      Post.where("post_name LIKE ?", "#{value}%")
-    elsif model == 'invite'
-      Invite.where("invite_name LIKE ?", "#{value}%")
-    end
-  end
-
-  def backward(model, value)
-    if model == 'post'
-      Post.where("post_name LIKE ?", "%#{value}")
-    elsif model == 'invite'
-      Invite.where("invite_name LIKE ?", "%#{value}")
-    end
-  end
-
   def partical(model, value)
     if model == 'post'
       Post.where("post_name LIKE ?", "%#{value}%")
+    elsif model == 'tag'
+      Post.includes(:tag_relationships, :tags).where(tags: {tag_name: value})
     elsif model == 'invite'
       Invite.where("invite_name LIKE ?", "%#{value}%")
+    elsif model == 'invite_tag'
+      Invite.includes(:invite_tag_relationships, :invite_tags).where(invite_tags: {invite_tag_name: value})
     end
   end
 
   def search_for(how, model, value)
-    case how
-    when 'match'
-      match(model, value)
-    when 'forward'
-      forward(model, value)
-    when 'backward'
-      backward(model, value)
-    when 'partical'
-      partical(model, value)
-    end
+    partical(model, value)
   end
 
 end
